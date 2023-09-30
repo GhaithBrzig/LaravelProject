f541699b64fe6688f087a0f58da11189799bd65f
Acil Farhat <acil.farhat@esprit.tn>
2023-09-30 18:51:25 +0100

    relation between tables done

--- app/Models/comment.php
+++ app/Models/comment.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class comment extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['content', 'user_id', 'post', 'datePosted', 'likes'];
+
+    
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Models/post.php
+++ app/Models/post.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'publicationDate', 'category', 'user_id', 'likes'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
