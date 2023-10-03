a4033e7372a42919bef9728645a948331d7ed490
rayen-creator <rayenoueslati60@gmail.com>
2023-10-03 13:22:50 +0100

    🐛 fixed vite causing error with font

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            overlay: false,
+        },
+    },
     plugins: [
         laravel({
             input: [
@@ -9,6 +14,7 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+
         }),
     ],
 });
