af4a089f4d249bbf41657b50d673e69a5fb52017
NourMakdouli <nour.makdouli@esprit.tn>
2023-10-22 23:58:18 +0100

    🐛  fix sql error

--- database/migrations/2023_10_22_164814_create_likes_table.php
+++ database/migrations/2023_10_22_164814_create_likes_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->unsignedBigInteger('post_id');
             $table->timestamps();
 
-            
+
              // Define foreign key constraints
              $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
              $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
